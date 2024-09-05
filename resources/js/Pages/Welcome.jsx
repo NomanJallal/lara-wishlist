435a19c473ee1c41f5c5efe4c0b592c0639c9db3
Noman Jalal <nomanjalaal@gmail.com>
2024-09-05 22:35:35 +0500

    Update Welcome page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -48,7 +48,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                     </div>
 
                     <div className="mt-16">
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+                        {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                             <a
                                 href="https://laravel.com/docs"
                                 className="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
@@ -287,14 +287,20 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                     </p>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
 
                     <div className="flex justify-center mt-16 px-6 sm:items-center sm:justify-between">
                         <div className="text-center text-sm sm:text-start">&nbsp;</div>
 
-                        <div className="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-end sm:ms-0">
-                            Laravel v{laravelVersion} (PHP v{phpVersion})
+                        <div className="text-center text-sm text-gray-500 dark:text-gray-400  sm:ms-0">
+                            <p>
+                                Laravel v{laravelVersion} (PHP v{phpVersion})
+                            </p>
+                            {/* <p className="mx-2">&middot;</p> */}
+                            <p>
+                            React v18 (Node v20)
+                            </p>
                         </div>
                     </div>
                 </div>
